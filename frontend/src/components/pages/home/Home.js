16c95f1806244bfed439af3832d28070ca1d6dfe
Macroshock <macroshock.games@gmail.com>
2019-12-30 01:07:40 -0300

    Finished skeleton of home page

--- frontend/src/components/pages/home/Home.js
+++ frontend/src/components/pages/home/Home.js
@@ -6,12 +6,46 @@ function Home() {
   return (
     <React.Fragment>
       <div className="image-container">
-          <div className="main-image">
-            
+        <div className="main-image">
+          <div className="header-container">
+            <span className="header-text-title"> Title </span>
+            <span className="header-text-subtitle"> subtitle </span>
+            <button className="header-button-getstarted"> Get started </button>
           </div>
         </div>
+      </div>
       
-      <h1>Home</h1>
+      <div className="main-section">
+        <div className="main-section-header">
+          <span className="main-section-title"> Title </span>
+          <span className="main-section-subtitle"> Subtitle </span>
+        </div>
+        <div className="main-section-body">
+          <div className="main-section-card">
+            text
+          </div>
+          <div className="main-section-card">
+            text
+          </div>
+          <div className="main-section-card">
+            text
+          </div>
+        </div>
+      </div>
+
+      <div className="secondary-section">
+      <div className="secondary-section-header">
+          <span className="secondary-section-title"> Title </span>
+          <span className="secondary-section-subtitle"> Subtitle </span>
+        </div>
+        <p className="secondary-section-body">
+          text
+        </p>
+      </div>
+
+      <div className="footer">
+        <span className="footer-text">text</span>
+      </div>
     </React.Fragment>
   )
 }
