cf61dea19856652fd91552ab4ebeb61d1927d17c
Macroshock <macroshock.games@gmail.com>
2020-01-18 01:34:28 -0300

    Finished the rest of the Users endpoints in the API

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
-const MongoClient = require('mongodb').MongoClient
+const MongoDb = require('mongodb')
+const MongoClient = MongoDb.MongoClient
 const assert = require('assert')
 const jwt = require('jsonwebtoken')
 const constants = require('../constants/constants.js')
@@ -10,7 +11,7 @@ const COLLECTION = 'Users'
 
 // Validate token middleware
 function validateToken(req, res, next) {
-  const header = req.headers["authorization"]
+  const header = req.headers['authorization']
 
   // check if the authorization header is missing
   if (!header) {
@@ -36,6 +37,31 @@ function validateToken(req, res, next) {
   })
 }
 
+// Middleware to verify that certain user exists
+function checkUser(req, res, next) {
+  
+  const client = new MongoClient(constants.DB_URL)
+  client.connect(function(err) {
+    assert.equal(null, err)
+  
+    const db = client.db(constants.DB_NAME)
+    const collection = db.collection(COLLECTION)
+
+    // Find user in database
+    collection.findOne({ _id: new MongoDb.ObjectID(req.params.id) }, function(err, result) {
+      assert.equal(null, err)
+
+      if (result == null) {
+        return res.status(404).json({ message: 'User not found' })
+      }
+
+      client.close()
+      next()
+    })
+  })
+
+}
+
 // Create user
 router.post('/', (req, res) => {
 
@@ -57,7 +83,8 @@ router.post('/', (req, res) => {
     collection.insertOne(user, function(err, result) {
       assert.equal(null, err)
       res.status(201).json(result)
-      console.log("User created")
+      console.log('User created')
+      client.close()
     })
   })
 
@@ -65,6 +92,23 @@ router.post('/', (req, res) => {
 
 // Get user
 router.get('/:id', (req, res) => {
+
+  const client = new MongoClient(constants.DB_URL)
+  client.connect(function(err) {
+    assert.equal(null, err)
+    console.log('Getting user')
+  
+    const db = client.db(constants.DB_NAME)
+    const collection = db.collection(COLLECTION)
+
+    // Find user in database
+    collection.findOne({ _id: new MongoDb.ObjectID(req.params.id) }, function(err, result) {
+      assert.equal(null, err)
+      res.status(200).json(result)
+      console.log(`User ${req.params.id} found`)
+      client.close()
+    })
+  })
   
 })
 
@@ -81,7 +125,7 @@ router.get('/', (req, res) => {
   const client = new MongoClient(constants.DB_URL)
   client.connect(function(err) {
     assert.equal(null, err)
-    console.log("Getting all users")
+    console.log('Getting all users')
   
     const db = client.db(constants.DB_NAME)
     const collection = db.collection(COLLECTION)
@@ -89,8 +133,9 @@ router.get('/', (req, res) => {
     // Find users in database
     collection.find({}).toArray(function(err, docs) {
       assert.equal(err, null)
-      res.json(docs)
-      console.log("Users fetched")
+      res.status(200).json(docs)
+      console.log('Users fetched')
+      client.close()
     })
   })
 
@@ -98,13 +143,66 @@ router.get('/', (req, res) => {
 
 
 // Update user
-router.patch('/:id', (req, res) => {
+router.patch('/:id', checkUser, (req, res) => {
   
+  let updatedUser = { $set: { } }
+
+  if (req.body.name) {
+    updatedUser.$set.name = req.body.name
+  } 
+
+  if (req.body.email) {
+    updatedUser.$set.email = req.body.email
+  } 
+
+  if (req.body.password) {
+    updatedUser.$set.password = req.body.password
+  } 
+
+  if (req.body.role) {
+    updatedUser.$set.role = req.body.role
+  } 
+
+  const client = new MongoClient(constants.DB_URL)
+  client.connect(function(err) {
+    assert.equal(null, err)
+    console.log('Updating user')
+  
+    const db = client.db(constants.DB_NAME)
+    const collection = db.collection(COLLECTION)
+
+    // Delete user in database
+    collection.updateOne({ _id: new MongoDb.ObjectID(req.params.id) }, updatedUser, 
+                          function(err, result) {
+      assert.equal(null, err)
+      res.sendStatus(200)
+      console.log('User updated')
+      client.close()
+    })
+  })
+
 })
 
 // Delete user
-router.delete('/:id', (req, res) => {
+router.delete('/:id', checkUser, (req, res) => {
+
+  const client = new MongoClient(constants.DB_URL)
+  client.connect(function(err) {
+    assert.equal(null, err)
+    console.log('Deleting user')
   
+    const db = client.db(constants.DB_NAME)
+    const collection = db.collection(COLLECTION)
+
+    // Delete user in database
+    collection.deleteOne({ _id: new MongoDb.ObjectID(req.params.id) }, function(err, result) {
+      assert.equal(null, err)
+      res.sendStatus(204)
+      console.log('User deleted')
+      client.close()
+    })
+  })
+
 })
 
 module.exports = router
